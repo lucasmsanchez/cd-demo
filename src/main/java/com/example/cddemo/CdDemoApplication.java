6ff4fa4bf11d0e0308302a7db04532a7d57caf02
Lucas <lucasmsanchez@protonmail.com>
2021-12-16 11:03:11 -0800

    new spring boot app

---
+++ src/main/java/com/example/cddemo/CdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdDemoApplication.class, args);
+	}
+
+}
